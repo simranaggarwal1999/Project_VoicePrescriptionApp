0047987d3ff34e159d5abe3de729d24ac34ff608
simranaggarwal1999 <simranaggarwal738@gmail.com>
2020-02-26 01:05:43 +0530

    change

---
+++ utilities/email.js
@@ -0,0 +1,25 @@
+const nodemailer = require("nodemailer");
+const appPassword=require("../configs/config").APP
+module.exports = async function (options) {
+    let mailTransporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.com",
+        auth: {
+            user: "simranaggarwal738@gmail.com",
+            pass: appPassword
+        }
+    });
+    let mailDetails = {
+        from: "simranaggarwal738@gmail.com",
+        to: options.recieversEmailId,
+        subject: options.subject,
+        html: options.html
+    };
+    try {
+        await mailTransporter.sendMail(mailDetails);
+        console.log("Email was send")
+    } catch (err) {
+        console.log(err);
+        console.log("Email could not be send");
+    }
+};
